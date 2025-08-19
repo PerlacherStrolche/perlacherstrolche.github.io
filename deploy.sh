b6df8bf973dccea48ddf6b9decb35eb6b70b140d
Thomas Linke <t.linke@pripares.com>
2025-08-19 08:48:53 +0200

    refactor: jobs

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rm -r ./web/
 
 bun run build
 
-scp -O -r web u63488074@home368972730.1and1-data.host:/kunden/homepages/29/d368972730/htdocs/
+scp -O -r web strolche.prod:/kunden/homepages/29/d368972730/htdocs/
